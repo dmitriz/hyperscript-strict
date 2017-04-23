b8ace738ee0df5d58a848ab944225a404a4d10d3
dmitriz <dmitri14@gmail.com>
2017-04-23 18:14:03 +0100

    Formatting

--- hyperscript-extend.js
+++ hyperscript-extend.js
@@ -5,9 +5,9 @@ var isObject = val => (null != val) && ('object' == typeof val)
 
 var hExt = (obj, ...rest) => isObject(obj) 
 	? h(obj.tag, obj.props, obj.children.map(child => isObject(child) 
-		? hExt(child) 
-		: child
-	))
+			? hExt(child) 
+			: child
+		))
 	: h(obj, ...rest)
 
 module.exports = hExt
